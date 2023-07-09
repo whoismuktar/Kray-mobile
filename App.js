69f7b120cd61ee1af1a60f456dcd469655368e15
Muktar Yusuf <mayunited12@yahoo.com>
2023-07-09 09:56:20 +0100

    custom fonts import

--- App.js
+++ App.js
@@ -3,14 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./Navigation";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Regular": require("./src/assets/fonts/inter/Inter-Regular.ttf"),
+    "Inter-Medium": require("./src/assets/fonts/inter/Inter-Medium.ttf"),
+    "Inter-Bold": require("./src/assets/fonts/inter/Inter-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
     <Provider store={store}>
       <StatusBar style="auto" />
       <Navigator />
     </Provider>
   );
+
 }
 
 const styles = StyleSheet.create({
