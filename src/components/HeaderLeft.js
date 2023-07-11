5dce1c969fa7b3c7a3abe0857aa7d80a9a505337
Muktar Yusuf <mayunited12@yahoo.com>
2023-07-11 09:46:14 +0100

    add logo, headerLeft component

---
+++ src/components/HeaderLeft.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Pressable } from "react-native";
+import { ChevronLeftIcon} from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
+
+function HeaderLeft(props) {
+  const navigation = useNavigation();
+  const propIcon = props.iwcon;
+
+  return (
+    <Pressable style={styles.goBackIcon} onPress={() => navigation.goBack()}>
+      {propIcon || (
+        <ChevronLeftIcon
+            size={24}
+          color={props.color || "#000000"}
+        />
+      )}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  goBackIcon: {
+    textAlign: "center",
+    alignSelf: "flex-start",
+  },
+});
+export default HeaderLeft;
