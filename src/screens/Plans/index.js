7ad9fc4b2cf1ed2c0b5d457550a38c85c8e28d62
Muktar Yusuf <mayunited12@yahoo.com>
2023-09-18 00:52:27 +0100

    fix scroll glitch

--- src/screens/Plans/index.js
+++ src/screens/Plans/index.js
@@ -1,9 +1,4 @@
-import {
-  View,
-  Keyboard,
-  TouchableWithoutFeedback,
-  FlatList,
-} from "react-native";
+import { View, FlatList } from "react-native";
 import { baseStyle } from "../../assets/styles/base";
 import Text from "../../components/Text";
 import Button from "../../components/Button";
@@ -13,13 +8,7 @@ import { TextInput } from "react-native";
 import PlanCard from "../../components/PlanCard";
 import { useSelector } from "react-redux";
 
-const HideKeyboard = ({ children }) => (
-  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    {children}
-  </TouchableWithoutFeedback>
-);
-
-function Plans({navigation}) {
+function Plans({ navigation }) {
   const { userPlans } = useSelector((state) => state.user);
   const filters = [
     {
@@ -41,85 +30,82 @@ function Plans({navigation}) {
   ];
 
   return (
-    <HideKeyboard>
-      <View style={baseStyle.page}>
-        <View style={{ flexDirection: "row" }}>
-          <Button
-            text="Create New Goal"
-            type="outlinedBtn"
-            minimal
-            style={{ marginRight: 20 }}
-            onPress={() => navigation.navigate("NewGoal")}
-          />
-          <Button
-            text="Create New Health Plan"
-            type="outlinedBtn"
-            minimal
-            onPress={() => navigation.navigate("NewPlan")}
-          />
-        </View>
-
-        <View style={{ marginVertical: 20 }}>
-          <Input
-            icon={
-              <MagnifyingGlassIcon
-                name="search"
-                size={24}
-                color={baseStyle.gray500}
-              />
-            }
-            rightIcon={<Button text="Search" minimal />}
-            hasLeftIcon
-            hasRightIcon
-            placeholder="Search goals or health plans"
-            style={{ paddingVertical: 17 }}
-          />
-        </View>
-
-        <View style={{}}>
-          <FlatList
-            keyExtractor={(item, index) => index}
-            data={filters}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            ItemSeparatorComponent={<View style={{ marginLeft: 10 }}></View>}
-            contentContainerStyle={{
-              alignItems: "stretch",
-              justifyContent: "space-between",
-              marginBottom: 20,
-            }}
-            renderItem={({ item, index }) => (
-              <Button
-                text={item.title}
-                type="outlinedBtn"
-                style={{ borderColor: baseStyle.gray200 }}
-                minimal
-              />
-            )}
-          />
-        </View>
-
-        <View style={baseStyle.section["fullWidth"]}>
-          <FlatList
-            keyExtractor={(item, index) => index}
-            data={userPlans}
-            vertical
-            ItemSeparatorComponent={<View style={{ marginBottom: 20 }}></View>}
-            
-            contentContainerStyle={{
-              // paddingTop: 20,
-              paddingBottom: 120,
-              width: "90%",
-              alignSelf: "center",
-              // height: "100%",
-              flexGrow: 1,
-              // backgroundColor: "red",
-          }}
-            renderItem={({ item, index }) => <PlanCard plan={item} />}
-          />
-        </View>
+    <View style={baseStyle.page}>
+      <View style={{ flexDirection: "row" }}>
+        <Button
+          text="Create New Goal"
+          type="outlinedBtn"
+          minimal
+          style={{ marginRight: 20 }}
+          onPress={() => navigation.navigate("NewGoal")}
+        />
+        <Button
+          text="Create New Health Plan"
+          type="outlinedBtn"
+          minimal
+          onPress={() => navigation.navigate("NewPlan")}
+        />
       </View>
-    </HideKeyboard>
+
+      <View style={{ marginVertical: 20 }}>
+        <Input
+          icon={
+            <MagnifyingGlassIcon
+              name="search"
+              size={24}
+              color={baseStyle.gray500}
+            />
+          }
+          rightIcon={<Button text="Search" minimal />}
+          hasLeftIcon
+          hasRightIcon
+          placeholder="Search goals or health plans"
+          style={{ paddingVertical: 17 }}
+        />
+      </View>
+
+      <View style={{}}>
+        <FlatList
+          keyExtractor={(item, index) => index}
+          data={filters}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          ItemSeparatorComponent={<View style={{ marginLeft: 10 }}></View>}
+          contentContainerStyle={{
+            alignItems: "stretch",
+            justifyContent: "space-between",
+            marginBottom: 20,
+          }}
+          renderItem={({ item, index }) => (
+            <Button
+              text={item.title}
+              type="outlinedBtn"
+              style={{ borderColor: baseStyle.gray200 }}
+              minimal
+            />
+          )}
+        />
+      </View>
+
+      <View style={baseStyle.section["fullWidth"]}>
+        <FlatList
+          keyExtractor={(item, index) => index}
+          data={userPlans}
+          vertical
+          ItemSeparatorComponent={<View style={{ marginBottom: 20 }}></View>}
+          contentContainerStyle={{
+            // paddingTop: 20,
+            paddingBottom: 120,
+            width: "90%",
+            alignSelf: "center",
+            // height: "100%",
+            flexGrow: 1,
+            // backgroundColor: "red",
+          }}
+          renderItem={({ item, index }) => <PlanCard plan={item} />}
+        />
+      </View>
+    </View>
   );
 }
 
